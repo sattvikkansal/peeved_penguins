f668defa26654bc7d87e33f810edf0f324c7dc72
Sattvik Kansal <sattvik@sattvik.attlocal.net>
2014-08-05 15:33:05 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Sattvik Kansal on 8/5/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
